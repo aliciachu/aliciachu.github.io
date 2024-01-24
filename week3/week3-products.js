9452b2d95494bde0dbf45f060bcf9f316a616c9f
Alicia Chu <nuyao73@hotmail.com>
2024-01-24 23:13:33 +0800

    week 3 done

---
+++ week3/week3-products.js
@@ -0,0 +1,114 @@
+import { createApp } from 'https://cdnjs.cloudflare.com/ajax/libs/vue/3.0.11/vue.esm-browser.js';
+
+const url = "https://ec-course-api.hexschool.io/v2/";
+const path = "amaaaa";
+
+let productModal = null;
+let delProductModal = null;
+const data = createApp({
+    data() {
+        return {
+            selectedProduct: {
+                imagesUrl: []
+            },
+            isActionAdd: false,
+            products: []
+        }
+    }, methods: {
+        checkLogin() {
+            axios.post(`${url}api/user/check`)
+                .then(res => {
+                    if (res.data.success) {
+                        this.getProduct();
+                    } else {
+                        alert(res.data.message);
+                        window.location = 'week2-login.html';
+                    }
+                })
+                .catch(err => {
+                    console.log(err);
+                })
+        },
+        getProduct() {
+            axios.get(`${url}api/${path}/admin/products/all`)
+                .then(res => {
+                    if (res.data.success) {
+                        //轉成array
+                        this.products = Object.values(res.data.products);
+                    } else {
+                        alert(res.data.message);
+                    }
+                })
+                .catch(err => {
+                    console.log(err);
+                })
+        },
+        openAddProductModal() {
+            this.selectedProduct = { imagesUrl: [] };
+            this.isActionAdd = true;
+            productModal.show();
+        },
+        openEditProductModal(item) {
+            this.selectedProduct = { ...item };
+            this.isActionAdd = false;
+            productModal.show();
+        },
+        openDeleteProductModal(item) {
+            this.selectedProduct = { ...item };
+            delProductModal.show();
+        },
+        addProduct() {
+            axios.post(`${url}api/${path}/admin/product`, { data: this.selectedProduct })
+                .then(res => {
+                    alert(res.data.message);
+                    productModal.hide();
+                    this.getProduct();
+                }).catch(err => {
+                    alert(err.data.message);
+                });
+        },
+        editProduct() {
+            axios.put(`${url}api/${path}/admin/product/${this.selectedProduct.id}`, { data: this.selectedProduct })
+                .then(res => {
+                    alert(res.data.message);
+                    productModal.hide();
+                    this.getProduct();
+                }).catch(err => {
+                    alert(err.data.message);
+                });
+        },
+        deleteProduct() {
+            axios.delete(`${url}api/${path}/admin/product/${this.selectedProduct.id}`)
+                .then(res => {
+                    alert(res.data.message);
+                    delProductModal.hide();
+                    this.getProduct();
+                }).catch(err => {
+                    alert(err.data.message);
+                });
+        },
+        addImage() {
+            //this.selectedProduct.imagesUrl = [];
+            this.selectedProduct.imagesUrl.push('');
+        },
+        deleteImage(index) {
+            this.selectedProduct.imagesUrl.splice(index, 1);
+        },
+
+    },
+    // computed: {
+    //     showAddImageButton() {
+    //         return this.selectedProduct.imagesUrl.length == 0;
+    //     }
+    // },
+    mounted() {
+        productModal = new bootstrap.Modal(document.getElementById('productModal'));
+        delProductModal = new bootstrap.Modal(document.getElementById('delProductModal'));
+        // 取得cookie
+        const token = document.cookie.replace(/(?:(?:^|.*;\s*)hexToken\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+        axios.defaults.headers.common['Authorization'] = token;
+
+        this.checkLogin();
+    }
+});
+data.mount('#app');
