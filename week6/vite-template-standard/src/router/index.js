6e49b1d5c059b86e23050cf8489e8ff305b1b794
Alicia Chu <nuyao73@hotmail.com>
2024-03-23 10:34:18 +0800

    finish week 6

--- week6/vite-template-standard/src/router/index.js
+++ week6/vite-template-standard/src/router/index.js
@@ -3,13 +3,50 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: () => import('../views/HomeView.vue')
+    component: () => import('../views/FrontView.vue'),
+    children: [
+      {
+        path: '/',
+        name: 'Home',
+        component: () => import('../views/HomeView.vue')
+      },
+      {
+        path: 'about',
+        name: 'About',
+        component: () => import('../views/AboutView.vue')
+      },
+      {
+        path: 'products',
+        name: 'Product',
+        component: () => import('../views/ProductsView.vue')
+      },
+      {
+        path: 'cart',
+        name: 'Cart',
+        component: () => import('../views/CartView.vue')
+      }
+    ]
   },
   {
-    path: '/about',
-    name: 'About',
-    component: () => import('../views/AboutView.vue')
+    path: '/login',
+    name: 'Login',
+    component: () => import('../views/LoginView.vue')
+  },
+  {
+    path: '/admin',
+    component: () => import('../views/dashboard/DashboardView.vue'),
+    children: [
+      {
+        path: 'products',
+        name: 'Products',
+        component: () => import('../views/dashboard/ProductsView.vue')
+      },
+      {
+        path: 'order',
+        name: 'Order',
+        component: () => import('../views/dashboard/OrderView.vue')
+      }
+    ]
   }
 ]
 
